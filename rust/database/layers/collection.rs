71248dc96e3a28471d5a33ab12ac3b3d2fb78204
Luis C. M. Holanda <luiscmholanda@gmail.com>
2022-11-21 19:09:21 -0300

    feat(db/layers): add basic Collection layer
    This commit adds a very basic "Collection" layer.
    
    A `Collection<E>` is similar to `BTreeMap<Key, E>`, where `Key: TuplePack`.
    The layer is responsible for providing an easy way of storing and
    fetching entities by key or by a key prefix (i.e. a parent).
    
    The implementation is so basic that we can't really use it in production
    code, we still need a few thing:
    
    1. A more controlled way of converting `ResourceName`s into the
    underlying tree key. In the current implementation, each call
    needs to convert the name into the correct tuple, which is very
    error prone.
    
    2. To be forward compatible with storage schema changes, we need
    to add a initial byte of "metadata". This byte can be used to
    provide information of what storage schema is used. Storage
    schemas provide support for nice features as value compression and
    encryption.
    3. Memory pooling for encoding buffers.
    
    These features will be added in the next commits.

---
+++ rust/database/layers/collection.rs
@@ -0,0 +1,109 @@
+use std::marker::PhantomData;
+
+use foundationdb::{
+    tuple::{Subspace, TuplePack},
+    RangeOption,
+};
+use pl_database::{DbError, DbResult, Tx};
+use prost::{DecodeError, Message};
+
+pub struct Collection<E> {
+    subspace: Subspace,
+    _marker: PhantomData<E>,
+    // TODO(mempool): use mempool to reduce allocation cost.
+    //
+    // Most of the operations in this type need to allocate
+    // at least the buffer to the key (sometimes multiple).
+    // The cost associated can be reduced by reusing memory
+    // buffers.
+}
+
+impl<E> Collection<E> {
+    /// Create a new static collection.
+    ///
+    /// Static collections are those that have a static name, and thus
+    /// we don't need to translate them into a short prefix value.
+    ///
+    /// When creating a static collection, try to keep its name small,
+    /// as improves the database performance.
+    pub fn from_static(name: &'static str) -> Self {
+        Self {
+            subspace: Subspace::from_bytes(name.as_bytes()),
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<E> Collection<E>
+where
+    E: Message + Default,
+{
+    /// Get an entity from the collection, returning `None` if the key is not present.
+    ///
+    /// # Errors
+    ///
+    /// This method returns an error if the database operation fails or if we can't
+    /// decode the database value into an instance of `E`. Note that, as we use protocol
+    /// buffers for the encoding, the later can be prevented by not making wire incompatible
+    /// changes to the entity.
+    pub async fn get(&self, tx: &Tx, key: &impl TuplePack) -> DbResult<Option<E>, DecodeError> {
+        let key = self.subspace.pack(key);
+        let Some(bytes) = tx.get(&key).await? else {
+            return Ok(None);
+        };
+
+        decode_to_entity(&bytes).map(Some)
+    }
+
+    /// Get a range of entities.
+    ///
+    /// # Errors
+    ///
+    /// Each value can return an error if the database returns an error at that point or
+    /// if we fail to decode the returned bytes into an instance of `E`. The same observations
+    /// in [`Self::get`] applies here.
+    pub async fn range<'t>(
+        &self,
+        tx: &'t Tx,
+        opts: RangeOption<'_>,
+    ) -> DbResult<Vec<E>, DecodeError> {
+        let mut range_elems = vec![];
+
+        tx.for_each_in_range(opts, |_, value| {
+            let res = decode_to_entity::<E>(value);
+
+            match res {
+                Err(err) => std::future::ready(Err(err)),
+                Ok(elem) => {
+                    range_elems.push(elem);
+                    std::future::ready(Ok(true))
+                }
+            }
+        })
+        .await?;
+
+        todo!()
+    }
+
+    /// Set the value of a specific key.
+    pub async fn set(&self, tx: &mut Tx, key: &impl TuplePack, value: &E) {
+        let bytes = value.encode_to_vec();
+        let key = self.subspace.pack(key);
+
+        tx.set(&key, &bytes)
+    }
+
+    /// Clear a specific value from the collection.
+    pub async fn clear(&self, tx: &mut Tx, key: &impl TuplePack) {
+        let key = self.subspace.pack(key);
+
+        tx.clear(&key)
+    }
+}
+
+fn decode_to_entity<E>(bytes: &[u8]) -> DbResult<E, DecodeError>
+where
+    E: Message + Default,
+{
+    E::decode(bytes).map_err(DbError::Abort)
+}
